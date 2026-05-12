4489fd5ea6d677c9e80396e8068b7fdb60065fef
Bonface Gekara <bonface248gekara@gmail.com>
2026-05-12 20:57:31 +0300

    another configuration

--- backend/index.js
+++ backend/index.js
@@ -19,6 +19,7 @@ app.use(cors({
 }));
 app.use(cookieParser());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/auth', authRoutes);
 app.use('/api/verifier', verifierRoutes);
